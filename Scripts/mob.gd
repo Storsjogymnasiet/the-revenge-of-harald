5b3c3bfb4519e00f79135c7d316543c5dd6b3567
Thomas <tomasson.vader@gmail.com>
2026-01-30 11:44:35 +0100

    I love programming! :3

--- Scripts/mob.gd
+++ Scripts/mob.gd
@@ -14,11 +14,18 @@ class Mob extends Entity:
 	func initialize_stats():
 		stats = mob_type.mob_type[type]
 
+@export var Goal: Node = null
 
+var mob = Mob.new("Slime")
 func _ready() -> void:
-	pass # Replace with function body.
+	$NavigationAgent2D.target_position = Goal.global_position 
 
+func _physics_process(delta: float) -> void:
+	if !$NavigationAgent2D.is_target_reached():
+			var nav_point_direction = to_local($NavigationAgent2D.get_next_path_position()).normalized()
+			velocity = nav_point_direction * mob.stats["Speed"]
+			move_and_slide()
 
-
-func _process(delta: float) -> void:
-	pass
+func _on_timer_timeout() -> void:
+	if $NavigationAgent2D.target_position != Goal.global_position: 
+		$NavigationAgent2D.target_position = Goal.global_position
